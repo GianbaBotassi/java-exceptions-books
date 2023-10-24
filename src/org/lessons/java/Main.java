378a64338b41f0c9740af0554f24eac0d465a590
GianbaBotassi <gianba89@gmail.com>
2023-10-24 17:07:16 +0200

    Bonus writer and reader

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,5 +1,9 @@
 package org.lessons.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -43,21 +47,55 @@ public class Main {
                 }
             }
             if (choice == 2) {
-
                 flagExit = false;
-                System.out.println("Ecco la tua biblioteca");
-                for (Book book : biblioteca
-                ) {
-                    if (book != null) {
-                        System.out.println(book.toString());
-                    }
-                }
+            }
+        }
+
+        scanner.close();
+
+        FileWriter biblioWriter = null;
+        try {
+            File biblioFile = new File("./biblioteca.txt");
+            if(biblioFile.exists()){
+                biblioFile.delete();
             }
 
-
+            biblioWriter = new FileWriter("./biblioteca.txt", true);
+            for (Book book : biblioteca
+            ) {
+                if (book != null) {
+                    biblioWriter.write(book.getTitolo() + "\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                biblioWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        biblioteca[0].setTitolo("");
-        scanner.close();
+
+
+        Scanner reader = null;
+        try {
+            File biblioReader = new File("./biblioteca.txt");
+            reader = new Scanner(biblioReader);
+            int counterBooks = 1;
+            System.out.println("Ecco la tua biblioteca: ");
+            while(reader.hasNextLine()){
+                String riga = reader.nextLine();
+                System.out.println(counterBooks + ") " + riga);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Il file non esiste.");
+        }finally {
+        reader.close();
+        }
+
+
+
 
     }
 }
