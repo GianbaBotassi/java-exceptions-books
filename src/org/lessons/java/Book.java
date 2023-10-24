a946387e8664da54a2f82bfb6552a1e51c624eea
GianbaBotassi <gianba89@gmail.com>
2023-10-24 16:10:37 +0200

    Add exceptions on costructor and setters

--- src/org/lessons/java/Book.java
+++ src/org/lessons/java/Book.java
@@ -1,4 +1,97 @@
 package org.lessons.java;
 
 public class Book {
+    //Attributi
+    private String titolo;
+    private String autore;
+    private String editore;
+    private int numPag;
+
+    //Costruttore
+    public Book(String titolo, String autore, String editore, int numPag) throws IllegalArgumentException{
+        if(titolo == null || titolo.isEmpty() || autore == null || autore.isEmpty() || editore == null || editore.isEmpty()){
+            throw new IllegalArgumentException("Il dato inserito non può essere vuoto.");
+        } else if (numPag <= 0) {
+            throw new IllegalArgumentException("Il libro deve avere almeno una pagina.");
+        }
+
+        this.titolo = titolo;
+        this.autore = autore;
+        this.editore = editore;
+        this.numPag = numPag;
+    }
+
+    //Metodi
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "titolo='" + titolo + '\'' +
+                ", autore='" + autore + '\'' +
+                ", editore='" + editore + '\'' +
+                ", numPag=" + numPag +
+                '}';
+    }
+
+    //Getters and Setters
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) throws IllegalArgumentException{
+        try {
+            if(titolo.isEmpty()){
+                throw new IllegalArgumentException("Hai provato a modificare il titolo ma non può essere vuoto.");
+            }
+            this.titolo = titolo;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) throws IllegalArgumentException {
+
+        try {
+            if(autore.isEmpty()){
+                throw new IllegalArgumentException("Hai provato a modificare l'autore ma non può essere vuoto.");
+            }
+            this.autore = autore;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getEditore() {
+        return editore;
+    }
+
+    public void setEditore(String editore) throws IllegalArgumentException{
+        try {
+            if(editore.isEmpty()){
+                throw new IllegalArgumentException("Hai provato a modificare l'autore ma non può essere vuoto.");
+            }
+            this.editore = editore;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public int getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(int numPag) throws IllegalArgumentException {
+        try {
+            if(numPag <= 0){
+                throw new IllegalArgumentException("Il libro deve avere almeno una pagina.");
+            }
+            this.numPag = numPag;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,7 +1,63 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Book[] biblioteca = new Book[50];
+        int counterBiblioteca = 0;
+        boolean flagExit = true;
+
+        while (flagExit) {
+            int choice = 0;
+            try {
+                System.out.println("Vuoi inserire un libro? Digita:\n" +
+                        "1 - Inserisci un libro.\n" +
+                        "2 - Vedi la tua biblioteca.");
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Seleziona un'opzione valida.\n");
+            }
+
+            if (choice == 1) {
+                try {
+                    System.out.println("Inserisci il titolo.");
+                    String titolo = scanner.nextLine();
+                    System.out.println("Inserisci l'autore.");
+                    String autore = scanner.nextLine();
+                    System.out.println("Inserisci l'editore.");
+                    String editore = scanner.nextLine();
+                    int numPag = 0;
+                    try {
+                        System.out.println("Inserisci il numero di pagine.");
+                        numPag = Integer.parseInt(scanner.nextLine());
+                    } catch (NumberFormatException e) {
+                        System.out.println("Inserisci un numero di pagine corretto.");
+                    }
+                    biblioteca[counterBiblioteca] = new Book(titolo, autore, editore, numPag);
+                    counterBiblioteca++;
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage() + "\n");
+                }
+            }
+            if (choice == 2) {
+
+                flagExit = false;
+                System.out.println("Ecco la tua biblioteca");
+                for (Book book : biblioteca
+                ) {
+                    if (book != null) {
+                        System.out.println(book.toString());
+                    }
+                }
+            }
+
+
+        }
+        biblioteca[0].setTitolo("");
+        scanner.close();
 
     }
 }
